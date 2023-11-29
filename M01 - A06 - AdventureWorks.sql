f5e49b35ab74f1a842b9de26b1512009157e4ace
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-11-29 15:52:33 -0300

    sql

--- M01 - A06 - AdventureWorks.sql
+++ M01 - A06 - AdventureWorks.sql
@@ -8,11 +8,11 @@
 use master
 go
 
-RESTORE FILELISTONLY FROM DISK = 'c:\Backup\AdventureWorksLT2022.bak'
+RESTORE FILELISTONLY FROM DISK = 'C:\Users\Developer\Downloads\SqlServer2022\AdventureWorksLT2022.bak'
 
-RESTORE DATABASE AdventureWorksLT FROM DISK = 'c:\Backup\AdventureWorksLT2022.bak' WITH stats=2,
-MOVE 'AdventureWorksLT2022_Data' TO 'C:\MSSQL_Data\AdventureWorksLT.mdf',
-MOVE 'AdventureWorksLT2022_Log'  TO 'C:\MSSQL_Data\AdventureWorksLT_log.ldf'
+RESTORE DATABASE AdventureWorksLT FROM DISK = 'C:\Users\Developer\Downloads\SqlServer2022\AdventureWorksLT2022.bak' WITH stats=2,
+MOVE 'AdventureWorksLT2022_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\AdventureWorksLT.mdf',
+MOVE 'AdventureWorksLT2022_Log'  TO 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\AdventureWorksLT_log.ldf'
 
 ALTER AUTHORIZATION ON DATABASE::AdventureWorksLT TO sa
 
